3ce5821df10251fc8be4d04f20a05f323cd32aa1
Asti22 <uun3461@gmail.com>
2025-11-07 13:56:04 +0700

    Menambahkan Box berisi ElevatedCard dengan padding, sudut membulat, dan elevasi untuk menampilkan konten form secara terpusat.

--- app/src/main/java/com/example/navigasitugas/view/FormulirPendaftaranScreen.kt
+++ app/src/main/java/com/example/navigasitugas/view/FormulirPendaftaranScreen.kt
@@ -1,18 +1,30 @@
 package com.example.navigasitugas.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 
@@ -56,6 +68,24 @@ fun FormulirPendaftaranScreen(
                 color = Color.White,
                 fontWeight = FontWeight.Bold
             )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            contentAlignment = Alignment.Center
+        ){
+            {
+                ElevatedCard (
+                    elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp),
+                    shape = RoundedCornerShape(dimensionResource(R.dimen.corner_radius)),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(dimensionResource(R.dimen.padding_small))
+                ){
+
+                }
+
         }
     }
 
