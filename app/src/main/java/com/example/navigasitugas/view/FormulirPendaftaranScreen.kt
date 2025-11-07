9b729d32df72925abf6bed330811d554b8bbc0f8
Asti22 <uun3461@gmail.com>
2025-11-07 13:47:18 +0700

    Menambahkan state untuk menyimpan input pengguna (nama, alamat, jenisKelamin, status) serta daftar pilihan genderList dan statusList menggunakan string resource.

--- app/src/main/java/com/example/navigasitugas/view/FormulirPendaftaranScreen.kt
+++ app/src/main/java/com/example/navigasitugas/view/FormulirPendaftaranScreen.kt
@@ -1,10 +1,36 @@
 package com.example.navigasitugas.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+
 
 @Composable
-fun FormulirPendaftaranScreen(){
+fun FormulirPendaftaranScreen(
     navController: NavController,
     modifier: Modifier = Modifier
+    ){
+    val context = LocalContext.current
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+
+    val genderList = listOf(
+        stringResource(id = R.string.gender_male),
+        stringResource(id = R.string.gender_female)
     )
-}
+    val statusList = listOf(
+        stringResource(id = R.string.status_janda),
+        stringResource(id = R.string.status_lajang),
+        stringResource(id = R.string.status_duda)
+    )
+
+    }
