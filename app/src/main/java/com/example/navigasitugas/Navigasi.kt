1522b43e27f1f670733a94209badbb0a87853b2f
Asti22 <uun3461@gmail.com>
2025-11-07 13:24:36 +0700

    Enum untuk rute navigasi

---
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -0,0 +1,11 @@
+package com.example.navigasitugas
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulir,
+    ListPeserta,
+    SelamatDatang
+}
+
