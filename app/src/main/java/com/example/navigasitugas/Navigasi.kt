9b0528c97418e1005d08bc5d5011553ecea123ad
Asti22 <uun3461@gmail.com>
2025-11-07 13:33:04 +0700

    dari Formulir → List Peserta

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -1,8 +1,12 @@
 package com.example.navigasitugas
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -26,6 +30,10 @@ fun DataApp(
             composable(route = Navigasi.Formulir.name) {
                 FormulirPendaftaranScreen(
                     onSubmitBtnClick = {
+                        navController.navigate(Navigasi.ListPeserta.name)
+                    }
+                )
         }
+
 
 }
